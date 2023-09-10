8ffcfa161bfe380a7a77662b444b25446acbe42e
Charlie Greenman <charlie@razroo.com>
2023-09-10 16:43:39 -0400

    Add code for angular-16.2.0 create-workspace

---
+++ apps/test-tyson-003-dev/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test-tyson-003-dev-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test-tyson-003-dev';
+}
